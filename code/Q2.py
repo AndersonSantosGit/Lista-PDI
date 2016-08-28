68be3b0261aa305a72cadb7e49d49da50382fe34
Anderson Santos <anderson.bcc.uag@gmail.com>
2016-08-28 14:59:28 -0300

    refatoração Q2

--- code/Q2.py
+++ code/Q2.py
@@ -1,29 +1,22 @@
-'''
-Created on 28 de set de 2015
-
-@author: Anderson Santos
-'''
 import cv2
 import math
-#from matplotlib import pyplot as plt
 
-gray = cv2.imread('../Imagens/knee-mri-white.jpg',0)
-imgPot = cv2.imread('../Imagens/knee-mri-white.jpg',0)
+gray = cv2.imread('../imagens/knee-mri-white.jpg', 0)
+imgPot = cv2.imread('../imagens/knee-mri-white.jpg', 0)
 
-y = 5  #Valor da potecia  BEST VALUES: 1 | 5 | 10 | 15 | 20 | 25 | 30 |
+y = 5  # Valor da potecia  BEST VALUES: 1 | 5 | 10 | 15 | 20 | 25 | 30 |
 c = 1
 
 for i in range(imgPot.shape[0]):
     for j in range(imgPot.shape[1]):
-        imgPot[i,j] = (c * math.pow(imgPot[i,j]/255.0,y))*255.0;
+        imgPot[i, j] = (c*math.pow(imgPot[i, j]/255.0, y))*255.0
 
-
-cv2.imshow('teste1',gray)
-cv2.imshow('teste2',imgPot)
+cv2.imshow('teste1', gray)
+cv2.imshow('teste2', imgPot)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-'''  
+'''
 plt.subplot(131),plt.imshow(gray, cmap = 'gray')
 plt.title('Imagem orginal'), plt.xticks([]), plt.yticks([])
 
